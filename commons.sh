433f13805f08dfe046b84ef0d94f03856001c451
Andrey Pavlenko <andrey.a.pavlenko@gmail.com>
2013-02-28 17:37:25 +0400

    commons.sh

---
+++ commons.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+: ${PPA_URL:='http://ppa.launchpad.net/aap'}
+: ${DIR:="$(cd "$(dirname "$0")" && pwd)"}
+DEPENDS="$DEPENDS git"
+
+[ ! -f "$HOME/.build.config" ] || . "$HOME/.build.config" && IGNORE_GLOBAL_CONFIG='true'
+[ ! -f "$DIR/build.config" ]   || . "$DIR/build.config" && IGNORE_CONFIG='true'
+
+: ${PPA_BUILDER:="$DIR/../ppa-builder"}
+: ${PPA_BUILDER_URL:='https://github.com/AndreyPavlenko/ppa-builder.git'}
+
+[ -d "$PPA_BUILDER" ] || git clone "$PPA_BUILDER_URL" "$PPA_BUILDER"
+. "$PPA_BUILDER/build.sh"
+
+update() {
+    _git_update "$SRC_URL"
+}
+
+_changelog() {
+    local cur_version="$(_cur_version "$1")"
+    _git_changelog "${cur_version##*~}" "$REV"
+}
